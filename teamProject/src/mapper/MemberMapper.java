5cb87d39801c9fe87bc0dc570c2af60633143bca
KimMinjung <seti05@naver.com>
2020-06-28 21:10:35 +0900

    mapper 빼먹은것 올림

--- teamProject/src/mapper/MemberMapper.java
+++ teamProject/src/mapper/MemberMapper.java
@@ -19,9 +19,19 @@ public interface MemberMapper {
 	public List<DailyExpense> selectDailyExpense(@Param("userKey")int userKey, @Param("expenseDate")LocalDate expenseDate);
 	public List<DailyIncome> selectDailyIncome(@Param("userKey")int userKey, @Param("incomeDate")LocalDate incomeDate);
 	
-	public List<DailyIncome> selectWeekIncome(@Param("userKey")int userKey
+	//주별로 사용한 금액과 벌어들인 금액을 리스트에 받아옴
+	public DailyIncome selectWeekIncome(@Param("userKey")int userKey
 			, @Param("weekDate")LocalDate weekDate);
-	public List<DailyExpense> selectWeekExpense(@Param("userKey")int userKey
+	public DailyExpense selectWeekExpense(@Param("userKey")int userKey
 			, @Param("weekDate")LocalDate weekDate);
 	
+	public List<DailyExpense> selectMonthExpense(@Param("userKey")int userKey
+			, @Param("startDate")LocalDate startDate,@Param("endDate")LocalDate endDate);
+	public List<DailyIncome> selectMonthIncome(@Param("userKey")int userKey
+			, @Param("startDate")LocalDate startDate,@Param("endDate")LocalDate endDate);
+	
+	public List<DailyExpense> selectYearExpense(@Param("userKey")int userKey
+			, @Param("year")int year);
+	public List<DailyIncome> selectYearIncome(@Param("userKey")int userKey
+			, @Param("year")int year);	
 }
