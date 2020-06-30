0df2be8206c36d4c4cb9a9af6337f5c839925287
Youngmin Park <230020@naver.com>
2020-07-01 00:12:37 +0900

    .

--- teamProject/src/controller/AssetOfMemberController.java
+++ teamProject/src/controller/AssetOfMemberController.java
@@ -8,15 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
-import com.mysql.cj.xdevapi.JsonParser;
 
 import dto.AssetOfMember;
 import service.AssetNewsService;
 import service.AssetOfMemberService;
+import service.StatisticsService;
 
 @Controller
 @RequestMapping("/asset")
@@ -27,28 +25,34 @@ public class AssetOfMemberController {
 
 	@RequestMapping("/view")
 	public String getMemberAsset(@ModelAttribute("userKey")int userKey, Model m) {
-		List<AssetOfMember> aomList = aomService.getAssetOfMember(userKey);
+		List<AssetOfMember> aomList = aomService.getAssetOfMember(userKey);	//userKey값에 해당하는 자산을 배열로 받음
 		int i = 0;
 		int sumAssets = 0;
 		while(aomList.size() > i) {
 			sumAssets += aomList.get(i).getAmount();
 			i++;
-		}
+		}	// 각 자산 합계를 계산
 
 		AssetNewsService ans = new AssetNewsService();
 		StringBuilder newsString = ans.getNews();
-		JSONObject jsnObject = new JSONObject(newsString.toString());
-		JSONArray jsonArray = jsnObject.getJSONArray("items");
+
+		JSONObject jsonObject = new JSONObject(newsString.toString());
+		JSONArray jsonArray = jsonObject.getJSONArray("items");
+		System.out.println(jsonObject);
+		System.out.println(jsonObject.getClass());
+
+		// jsonArray는 네이버 자산관련 기사 10개를 json 배열로 받은 값
+		// jsp에서 출력할 방법 찾는 중 (혹시 방법 알고 계신 분은 도움 부탁드립니다)
 
 		m.addAttribute("aomList", aomList);
 		m.addAttribute("sumAsset", sumAssets);
 		m.addAttribute("newsList", jsonArray);
-		System.out.println(jsonArray.getClass());
 		return "showAsset";
 	}
 
 	@RequestMapping("/add")
 	public String showAddForm(@ModelAttribute("userKey")int userKey, Model m) {
+		
 		List<AssetOfMember> aomList = aomService.getAssetOfMember(userKey);
 		int i = 0;
 		int sumAssets = 0;
@@ -56,6 +60,9 @@ public class AssetOfMemberController {
 			sumAssets += aomList.get(i).getAmount();
 			i++;
 		}
+		//위 코드는 자산 추가 페이지에서 현재 보유 자산 보여주기 위한 것인데
+		//일단 코드만 두고 jsp에서의 출력은 보류합니다.
+		
 		m.addAttribute("aomList", aomList);
 		m.addAttribute("sumAsset", sumAssets);
 		return "addAssetForm";
@@ -65,16 +72,14 @@ public class AssetOfMemberController {
 	public String addAsset(@ModelAttribute("userKey")int userKey, Model m, AssetOfMember aom) {
 		String assetType = aom.getType();
 		int amount = aom.getAmount();
-		System.out.println(assetType);
-		System.out.println(amount);
-		if (assetType.equals("부채")) {
+		if (assetType.equals("부채")) {	//자산타입이 "부채"인 경우 마이너스 값으로 변경한 다음 db저장
 			amount*=-1;
 		}
-		System.out.println(amount);
 		aom.setAmount(amount);
 		aomService.addAsset(aom);
+		System.out.println(aom);
 		m.addAttribute("aom", aom);
-		return "addResult";
+		return "addAssetResult";
 	}
 
 	@RequestMapping("/edit")
@@ -88,12 +93,12 @@ public class AssetOfMemberController {
 	public String editAsset(int memAssetId, Model m,  AssetOfMember aom) {
 		String assetType = aom.getType();
 		int amount = aom.getAmount();
-		if (assetType.equals("부채")) {
+		if (assetType.equals("부채")) {	//자산 수정할때도 마찬가지로 부채인 경우 마이너스 처리
 			amount*=-1;
 		}
 		aom.setAmount(amount);
 		aomService.editAsset(aom);
-		return "editResult";
+		return "editAssetResult";
 	}
 
 	@RequestMapping("/delete")
@@ -103,8 +108,9 @@ public class AssetOfMemberController {
 		String bank = aom.getAssetsName();
 		String assetType = aom.getType();
 		String delResult = bank+"의 "+assetType+" "+amount+"원 항목이 삭제되었습니다.";
+		//자산 삭제 전, 삭제되는 자산정보 문자열로 저장후 메서드 돌고 나서 출력
 		m.addAttribute("delrst", delResult);
 		aomService.delAsset(memAssetId);
-		return "deleteResult";
+		return "deleteAssetResult";
 	}
 }
