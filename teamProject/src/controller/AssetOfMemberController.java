afe6f96004188ee24bd3d9459d3d488c6c49e60c
ympkor <63386212+ympkor@users.noreply.github.com>
2020-07-02 15:38:16 +0900

    Update AssetOfMemberController.java
    뉴스부분 수정 및 정리

--- teamProject/src/controller/AssetOfMemberController.java
+++ teamProject/src/controller/AssetOfMemberController.java
@@ -11,60 +11,73 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import dto.AssetOfMember;
 import service.AssetNewsService;
 import service.AssetOfMemberService;
-import service.StatisticsService;
 
 @Controller
 @RequestMapping("/asset")
-@SessionAttributes("userKey")
+@SessionAttributes({"userKey", "memAssetId"})
 public class AssetOfMemberController {
 
 	@Autowired AssetOfMemberService aomService;
 
 	@RequestMapping("/view")
-	public String getMemberAsset(@ModelAttribute("userKey")int userKey, Model m) {
+	public String getMemberAsset(@ModelAttribute("userKey") int userKey, 
+			Model m) throws JsonProcessingException {
 		List<AssetOfMember> aomList = aomService.getAssetOfMember(userKey);	//userKey값에 해당하는 자산을 배열로 받음
 		int i = 0;
+
 		int sumAssets = 0;
+		int sumDebt = 0;
 		while(aomList.size() > i) {
-			sumAssets += aomList.get(i).getAmount();
+			if (aomList.get(i).getAmount() > 0) {
+				sumAssets += aomList.get(i).getAmount();
+			}else if (aomList.get(i).getAmount() < 0) {
+				sumDebt += aomList.get(i).getAmount();
+			}
 			i++;
-		}	// 각 자산 합계를 계산
+		}
+		int sumTotal = sumAssets + sumDebt;
 
 		AssetNewsService ans = new AssetNewsService();
-		StringBuilder newsString = ans.getNews();
+		ObjectMapper mapper = new ObjectMapper();
+		String newsListJ = mapper.writeValueAsString(ans);
 
+		StringBuilder newsString = ans.getNews();
 		JSONObject jsonObject = new JSONObject(newsString.toString());
 		JSONArray jsonArray = jsonObject.getJSONArray("items");
-		System.out.println(jsonObject);
-		System.out.println(jsonObject.getClass());
-
-		// jsonArray는 네이버 자산관련 기사 10개를 json 배열로 받은 값
-		// jsp에서 출력할 방법 찾는 중 (혹시 방법 알고 계신 분은 도움 부탁드립니다)
 
 		m.addAttribute("aomList", aomList);
+		m.addAttribute("sumTotal", sumTotal);
 		m.addAttribute("sumAsset", sumAssets);
-		m.addAttribute("newsList", jsonArray);
+		m.addAttribute("sumDebt", sumDebt);
+		m.addAttribute("newsArr", jsonArray);
 		return "showAsset";
 	}
 
 	@RequestMapping("/add")
 	public String showAddForm(@ModelAttribute("userKey")int userKey, Model m) {
-		
+
 		List<AssetOfMember> aomList = aomService.getAssetOfMember(userKey);
+
 		int i = 0;
 		int sumAssets = 0;
+
 		while(aomList.size() > i) {
 			sumAssets += aomList.get(i).getAmount();
 			i++;
-		}
+		}	// 각 자산 합계를 계산
+
 		//위 코드는 자산 추가 페이지에서 현재 보유 자산 보여주기 위한 것인데
 		//일단 코드만 두고 jsp에서의 출력은 보류합니다.
-		
+
 		m.addAttribute("aomList", aomList);
 		m.addAttribute("sumAsset", sumAssets);
+
 		return "addAssetForm";
 	}
 
@@ -77,7 +90,6 @@ public class AssetOfMemberController {
 		}
 		aom.setAmount(amount);
 		aomService.addAsset(aom);
-		System.out.println(aom);
 		m.addAttribute("aom", aom);
 		return "addAssetResult";
 	}
@@ -85,6 +97,9 @@ public class AssetOfMemberController {
 	@RequestMapping("/edit")
 	public String showeditForm(int memAssetId, Model m, AssetOfMember aom) {
 		aom = aomService.getAssetById(memAssetId);
+		String assetType = aom.getAssetsName();
+
+		System.out.println(assetType);
 		m.addAttribute("aom", aom);
 		return "editAssetForm";
 	}
@@ -109,7 +124,7 @@ public class AssetOfMemberController {
 		String assetType = aom.getType();
 		String delResult = bank+"의 "+assetType+" "+amount+"원 항목이 삭제되었습니다.";
 		//자산 삭제 전, 삭제되는 자산정보 문자열로 저장후 메서드 돌고 나서 출력
-		m.addAttribute("delrst", delResult);
+		m.addAttribute("delStr", delResult);
 		aomService.delAsset(memAssetId);
 		return "deleteAssetResult";
 	}
