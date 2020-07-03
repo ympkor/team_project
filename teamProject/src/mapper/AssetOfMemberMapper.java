e2a475b6faeaf649e6dfb34ec15c3b32228abe6a
ympkor <63386212+ympkor@users.noreply.github.com>
2020-07-03 14:24:03 +0900

    Update AssetOfMemberMapper.java
    자산-수입지출 연동관련 추가

--- teamProject/src/mapper/AssetOfMemberMapper.java
+++ teamProject/src/mapper/AssetOfMemberMapper.java
@@ -6,9 +6,11 @@ import dto.AssetOfMember;
 
 public interface AssetOfMemberMapper {
 
-	public List<AssetOfMember> selectAomById(int userKey);
+	public List<AssetOfMember> selectAssetListById(int userKey);
 	
 	public AssetOfMember getAssetById(int memAssetId);
+
+	public AssetOfMember getLastAssetByUserKey(int userKey);
 
 	public void addAsset(AssetOfMember aom);
 
@@ -17,4 +19,8 @@ public interface AssetOfMemberMapper {
 	public void delAsset(int memAssetId);
 	//탈퇴시 해당 유저키에 있는 자산을 모두 지움
 	public void delAssetByuserkeyAll(int userKey);
+	
+	public void addAssetToIncome(AssetOfMember aom);
+
+	public void addAssetToExpense(AssetOfMember aom);
 }
